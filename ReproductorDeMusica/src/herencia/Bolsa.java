7bc2d481e426f1fffec33ad6ce0d54a0f7b3901c
Pablo <29169516+PabloDV343@users.noreply.github.com>
2019-01-18 19:37:41 -0600

    finished

--- ReproductorDeMusica/src/herencia/Bolsa.java
+++ ReproductorDeMusica/src/herencia/Bolsa.java
@@ -22,4 +22,26 @@ public class Bolsa {
     public ArrayList<Producto> dameTodos(){
         return cuenta;
     }
+    
+    public int llenar(String[] lista){
+        int cantidad = (int) (Math.random() * 5) + 1;
+        
+        for(int i = 0; i < cantidad; i++){
+            if((int) (Math.random() * 3) == 1){
+                int precio = (int) (Math.random() * 501);
+                
+                echar(new Producto(precio, "src/herencia/" +
+                      lista[(int) (Math.random() * lista.length)]));
+            }
+            else{
+                int precio = (int) (Math.random() * 501);
+                int descuento = (int) (Math.random() * 101);
+                
+                echar(new DeOferta(precio, "src/herencia/" +
+                      lista[(int) (Math.random() * lista.length)],
+                      descuento));
+            }
+        }
+        return cantidad;
+    }
 }
--- ReproductorDeMusica/src/herencia/Dibujo.java
+++ ReproductorDeMusica/src/herencia/Dibujo.java
@@ -4,116 +4,134 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 
 public class Dibujo extends JApplet {
 
     Bolsa bolsa;
-    ArrayList<String> subTotal;
-    
+    String[] lista;
+
     @Override
     public void init() {
-        //this.setSize(500, 500);
+        this.setSize(450, 200);
+
+        lista = new String[]{"Jugo2.png"};
+
         bolsa = new Bolsa();
-        
-        for(int i = 0; i < 5; i++){
-            if((int) Math.random() * 3 < 1){
-                int precio = (int) Math.random() * 501;
-                
-                bolsa.echar(new Producto(precio, "src/herencia/Jugo2.png"));
-            }
-            else{
-                int precio = (int) Math.random() * 501;
-                int descuento = (int) Math.random() * 101;
-                
-                bolsa.echar(new DeOferta(precio, "src/herencia/Jugo2.png",
-                            descuento));
-            }
-        }
+        System.out.println("Numero de articulos: " + bolsa.llenar(lista));
     }
-    
+
+    public void drawLine(Graphics plumon) {
+        plumon.setColor(Color.BLACK);
+        plumon.fillRect(10, 100, 200, 20);
+    }
+
+    public void drawBag(Graphics plumon) {
+        plumon.setColor(Color.BLUE);
+        plumon.fillRect(200, 100, 60, 100);
+
+        plumon.setColor(Color.WHITE);
+        plumon.setFont(new Font("Arial", Font.ITALIC, 12));
+        plumon.drawString("Gual*Mart", 200, 150);
+    }
+
+    public float drawPrice(Graphics plumon, Producto p, float total) {
+        plumon.setColor(Color.WHITE);
+        plumon.fillRect(280, 0, 170, 100);
+
+        plumon.setColor(Color.BLACK);
+        plumon.drawString(p.getImagen().substring(p.getImagen().lastIndexOf("/") + 1, p.getImagen().length() - 4), 280, 15);
+        
+        System.out.print(p.getImagen().substring(p.getImagen().lastIndexOf("/") + 1, p.getImagen().length() - 4) + " ");
+        
+        plumon.setColor(Color.RED);
+        plumon.drawString("Costo: $"
+                + String.valueOf(p.getPrecio()), 280, 30);
+        
+        System.out.print("$" + p.getPrecio() + " ");
+
+        if (p instanceof DeOferta) {
+            plumon.setColor(Color.GREEN);
+            
+            plumon.drawString("Descuento: " + ((DeOferta) p).getDescuento() + "%",
+                    280, 45);
+            
+            plumon.drawString("Precio final: $" + ((DeOferta) p).calcularPrecio(),
+                    280, 60);
+            
+            System.out.println("- $" + (p.getPrecio() - ((DeOferta) p).calcularPrecio()) + "\t= $" + ((DeOferta) p).calcularPrecio());
+            
+            plumon.setColor(Color.BLACK);
+            plumon.drawString("Total: $" + String.valueOf(total + ((DeOferta) p).calcularPrecio()), 280, 80);
+        
+        }
+
+        
+        if(p instanceof DeOferta) return ((DeOferta) p).calcularPrecio();
+        
+        plumon.setColor(Color.BLACK);
+        plumon.drawString("Total: $" + String.valueOf(total + p.getPrecio()), 280, 80);
+        System.out.println();
+        return p.getPrecio();
+    }
+
+    public void drawMaualJuice(Graphics plumon, int x) {
+        //dibujo manual
+        plumon.setColor(Color.YELLOW);
+        plumon.fillRect(x, 10, 45, 75);
+        plumon.setColor(Color.WHITE);
+        plumon.drawString("Jugo", x + 7, 25);
+        plumon.drawLine(x, 35, x + 45, 35);
+        plumon.setColor(Color.ORANGE);
+        plumon.fillOval(x + 15, 50, 20, 20);
+    }
+
     @Override
-    public void paint(Graphics plumon){
-        
-        for(Producto p : bolsa.dameTodos())
-            System.out.println(p.getImagen());
-        
-        for(Producto producto : bolsa.dameTodos()){
-            
-            int d = 0;
-            int p = producto.getPrecio();
-            String imagen = producto.getImagen();
-            
-            if(producto instanceof DeOferta){
-                d = ((DeOferta) producto).getDescuento();
-            }
-            
-            subTotal.add(imagen + " $" + String.valueOf(p) + " $" + String.valueOf(d));
-            
+    public void paint(Graphics plumon) {
+
+        float total = 0;
+
+        for (Producto producto : bolsa.dameTodos()) {
+
             Image img;
             img = new ImageIcon(producto.getImagen()).getImage();
 
-            plumon.setColor(Color.BLACK);
-            plumon.fillRect(10, 100, 200, 20);
+            drawLine(plumon);
+            drawBag(plumon);
 
-            plumon.setColor(Color.BLUE);
-            plumon.fillRect(200, 100, 60, 100);
-
-            plumon.setColor(Color.WHITE);
-            plumon.setFont(new Font("Arial", Font.ITALIC, 12));
-            plumon.drawString("Gual*Mart", 200, 150);
-
-            for(int i = 1, x = 10; i <= 40; i++, x+=5){
-                
-    //            plumon.setColor(Color.YELLOW);
-    //            plumon.fillRect(x, 10, 45, 75);
-    //            plumon.setColor(Color.WHITE);
-    //            plumon.drawString("Jugo", x + 7, 25);
-    //            plumon.drawLine(x, 35, x + 45, 35);
-    //            plumon.setColor(Color.ORANGE);
-    //            plumon.fillOval(x + 15, 50, 20, 20);
+            for (int i = 1, x = 10; i <= 40; i++, x += 5) {
 
                 plumon.drawImage(img, x, 10, this);
 
-                try{
-                    Thread.sleep(190);
-                }catch(Exception e){
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
                 }
 
                 plumon.setColor(Color.WHITE);
                 plumon.fillRect(x - 1, 10, 47, 90);
             }
 
-
             //Applet.newAudioClip(Dibujo.class.getResource());
+            total += drawPrice(plumon, producto, total);
 
+            for (int y = 10; y < 100; y += 5) {
 
-            for(int y = 10; y < 100; y+=5){
-                
-                plumon.setColor(Color.BLUE);
-                for(String s : subTotal){
-                    System.out.println(s);
-                }
-                
                 plumon.drawImage(img, 210, y, this);
 
-                plumon.setColor(Color.BLUE);
-                plumon.fillRect(200, 100, 60, 100);
-
-                plumon.setColor(Color.WHITE);
-                plumon.setFont(new Font("Arial", Font.ITALIC, 12));
-                plumon.drawString("Gual*Mart", 200, 150);
-
-                try{
-                    Thread.sleep(190);
-                }catch(Exception ex){
+                drawBag(plumon);
+                try {
+                    Thread.sleep(100);
+                } catch (Exception ex) {
                     System.err.println(ex.getMessage());
                 }
+
                 plumon.setColor(Color.WHITE);
-                plumon.fillRect( 209, y-1, 47, 91-y);
+                plumon.fillRect(209, y - 1, 47, 91 - y);
             }
         }
+        
+        System.out.println("Total: $" + total);
     }
 }
